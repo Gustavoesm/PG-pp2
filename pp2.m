38b6c59ea76f3c12cea96c241d562ca683b20655
Gustavo Eugênio <gustavo.eugenio@hotmail.com>
2021-05-08 18:19:51 -0300

    generating skeletons

--- pp2.m
+++ pp2.m
@@ -9,12 +9,30 @@ pkg load image
 % Leonardo Melo Martins 726558
 % Luiz Felipe Guimarães 743570
 
-srcimg = imread("img/0.jpg");
-imbw = im2bw(srcimg);
-% imcompl = imcomplement(imbw);
+for iterator = 0:9
+	img_url = strcat("img/", num2str(iterator), ".jpg");
+	img_src = imread(img_url);
+	img_binary = im2bw(img_src);
+	img_compl = imcomplement(img_binary);
 
-Inf = 100; %ajustar estes valores
+	inf = 20; % adjust values
 
-bwesq = bwmorph(imbw,'skel',Inf);
+	img_skel = bwmorph(img_compl, 'skel', inf);
 
-imwrite(bwesq, "output.jpg");
+	output_url = strcat("output/", num2str(iterator), "-skeleton.jpg");
+	imwrite(img_skel, output_url);
+endfor
+
+%{ 
+------> TODO: 
+
+--> Considerando os números de 0 a 9, efetue a 
+representação/descrição de cada elemento pelo 
+descritor mais apropriado abordado em aula e/ou do 
+capítulo Representação e Descrição do Gonzalez.
+
+--> A saída do arquivo será em tela mostrando cada 
+número considerado, o descritor empregado e o seu 
+respectivo resultado.
+
+}%
