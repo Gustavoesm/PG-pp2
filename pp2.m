8df39c83ff117f02cbb0ece8ad6cf21651244bd4
Gustavo Eugênio <gustavo.eugenio@hotmail.com>
2021-05-08 22:35:50 -0300

    added scalar descriptors

--- pp2.m
+++ pp2.m
@@ -9,17 +9,31 @@ pkg load image
 % Leonardo Melo Martins 726558
 % Luiz Felipe Guimarães 743570
 
+# iterate through numbers 0-9
 for iterator = 0:9
+
+	### PRE-PROCESSING ###
+	# generate an string url path for each number
 	img_url = strcat("img/", num2str(iterator), ".jpg");
+	# load the image
 	img_src = imread(img_url);
+	# convert image to binary (also converts to grayscale before)
 	img_binary = im2bw(img_src);
+	# transform image and gets it's negative
 	img_compl = imcomplement(img_binary);
 
-	inf = 20; % adjust values
+	### SCALAR DESCRIPTORS ###
+	props = regionprops(img_compl, "Area", "EulerNumber", "Eccentricity", "Perimeter");
+	props.("Compactness") = props.("Perimeter") / props.("Area");
+	disp(props);
+	continue;
 
-	img_skel = bwmorph(img_compl, 'skel', inf);
-
+	### SKELETON ###
+	# skeleton transformation
+	img_skel = bwmorph(img_compl, 'skel', Inf);
+	# generate an output path and name to the skeleton
 	output_url = strcat("output/", num2str(iterator), "-skeleton.jpg");
+	# write image down to disk
 	imwrite(img_skel, output_url);
 endfor
 
@@ -34,5 +48,25 @@ capítulo Representação e Descrição do Gonzalez.
 --> A saída do arquivo será em tela mostrando cada 
 número considerado, o descritor empregado e o seu 
 respectivo resultado.
+
+----> REPRESENTAÇÕES POR FRONTEIRA
+
+–> Aproximações Poligonais
+–> Sequência de Segmentos
+–> Inscrição de Polígono Convexo
+–> Divisão Recursiva do Contorno
+–> Código da Cadeia
+–> Assinaturas
+–> Algoritmo MPP (Opcional)
+
+----> REPRESENTAÇÕES POR BORDA
+
+-> Esqueletonização
+-> Descritores Escalares:
+	– Área
+	– Número de Euler;
+	– Excentricidade;
+	– Compacidade;
+	
 
 }%
